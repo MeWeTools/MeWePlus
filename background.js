465cd2c0967c34275498f0c8580b611028ef5e30
ReimarPB <reimarpb@gmail.com>
2023-07-20 20:35:21 +0200

    Adapt to changes for site redesign

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 browser.webRequest.onBeforeRequest.addListener(interceptScripts, { urls: ["https://mewe.com/*"], types: ["script"] }, ["blocking"]);
 browser.webRequest.onBeforeRequest.addListener(interceptPurchases, { urls: ["https://mewe.com/api/v2/store/items/purchased"] }, ["blocking"]);
 
-let themes = [];
-
 function interceptScripts(req) {
 
 	const filter = browser.webRequest.filterResponseData(req.requestId);
@@ -22,10 +20,6 @@ function interceptScripts(req) {
 		response = response
 			.replace("this.get('globals.currentUser.id') === '5602c780e4b08f388c897a39'", "true")
 			.replace("getPrimaryEmail();", "true");
-
-		// Get themes
-		const themesMatch = response.match(/JSON\.parse\('(\{.+?\})'\);\\n\\n\/\/# sourceURL=webpack:\/\/mewe\/\.\/utils\/rev-manifest\.json/);
-		if (themesMatch) themes = JSON.parse(themesMatch[1].replace(/\\/g, ""));
 
 		filter.write(encoder.encode(response));
 		filter.disconnect();
@@ -51,10 +45,8 @@ async function interceptPurchases(req) {
 		result.items.push({ itemId: "emoji-" + name, expires: 9999999999 });
 	});
 
-	// Add themes
-	Object.keys(themes).forEach(theme => {
-		result.items.push({ itemId: theme.replace(".css", ""), expires: 9999999999 });
-	});
+	// Add dark theme
+	result.items.push({ itemId: "theme-dark", expires: 9999999999 });
 
 	const filter = browser.webRequest.filterResponseData(req.requestId);
 	const encoder = new TextEncoder();
